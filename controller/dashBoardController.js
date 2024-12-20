55486c059ac8aa491a5701f5fc3783fac2512e88
Chirag09021997 <chirag.somecodex@gmail.com>
2024-12-20 22:33:00 +0530

    changes added

--- controller/dashBoardController.js
+++ controller/dashBoardController.js
@@ -1,8 +1,26 @@
+const { Sequelize } = require("sequelize");
 const { renderPage } = require("../controller/commonController");
+const { market: MarketModel } = require("../models/index");
 const dashboard = async (req, res) => {
-  renderPage(req, res, "dashboard", {
-    title: "Dashboard",
-    activePage: "dashboard",
-  });
+  try {
+    const getData = await MarketModel.findAll({
+      attributes: [
+        "type",
+        [Sequelize.fn("COUNT", Sequelize.col("type")), "count"],
+      ],
+      group: ["type"],
+      raw: true,
+    });
+    console.log(`marketCounts => ${JSON.stringify(getData)}`);
+
+    renderPage(req, res, "dashboard", {
+      title: "Dashboard",
+      activePage: "dashboard",
+      getData,
+    });
+  } catch (error) {
+    console.error("Error fetching dashboard:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
 module.exports = { dashboard };
